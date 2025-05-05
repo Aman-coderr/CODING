8becab6d781099ed63077ecf31859bb5e1933711
Aman-coderr <mahatoaman2510@gmail.com>
2025-05-05 19:47:47 +0530

    Delete C directory

--- C/firstprogram.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    printf("hello coders\n");
-    int x;
-    x=5;
-    printf("%d",x);
-    return 0;
-}
--- C/vol.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    float r=5;
-    float v=4*22*r*r*r/21;
-    printf("Volume of the sphere is:%f",v);
- return 0;
-
-}
